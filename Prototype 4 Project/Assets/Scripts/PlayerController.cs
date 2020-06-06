ab5e29ce22eda997712d073aa0c37f990609e154
ellacreid <59098975+ellacreid@users.noreply.github.com>
2020-06-05 22:32:55 -0700

    Project Update 2

--- Prototype 4 Project/Assets/Scripts/PlayerController.cs
+++ Prototype 4 Project/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
     public bool hasPowerUp;
     public bool hasPowerUpTwo;
     public GameObject powerupIndicator;
+    public GameObject powerupIndicatorTwo;
+
+    private int height = 5;
+    private float floatForce = 25.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,21 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        powerupIndicatorTwo.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (this.transform.position.y < height)
+        {
+            // While space is pressed and player is low enough, float up
+            if (Input.GetKey(KeyCode.Space) && hasPowerUpTwo && this.transform.position.y < height)
+            {
+                playerRb.AddForce(Vector3.up * floatForce);
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, height);
+            playerRb.AddForce(-Vector3.up * floatForce / 10);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,7 +61,7 @@ public class PlayerController : MonoBehaviour
             hasPowerUpTwo = true;
             Destroy(other.gameObject);
             StartCoroutine(PowerUpCountDownRoutineTwo());
-            powerupIndicator.gameObject.SetActive(true);
+            powerupIndicatorTwo.gameObject.SetActive(true);
         }
     }
 
@@ -57,7 +76,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(10);
         hasPowerUpTwo = false;
-        powerupIndicator.gameObject.SetActive(false);
+        powerupIndicatorTwo.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
